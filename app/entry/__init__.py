3f15fdc869306007e8932cb8c8c882db891f8aef
Pedro Baumann <ondoheer@gmail.com>
2015-04-02 00:44:30 -0500

    added bcrypt

--- app/entry/__init__.py
+++ app/entry/__init__.py
@@ -1,3 +1,4 @@
+import bcrypt
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from forms import LoginForm
@@ -5,13 +6,20 @@ from forms import LoginForm
 entry = Blueprint("entry", __name__, template_folder="templates")
 
 
+
+
+
+
 @entry.route("/login", methods=["POST", "GET"])
 def login():
 	form = LoginForm()
+	salt = bcrypt.gensalt()
 
 	if form.validate_on_submit():
 		flash("Has hecho login con el user: {}".format(form.username.data))
-		print "validated"
+		user = form.username
+		password = bcrypt.hashpw(form.password.data, salt)
+		
 		return redirect(url_for("manager.index"))
 	else:
 		print "not validated"
